9776626ba43bee08431fa29cc5a6ee889e583a49
mohite2 <54907173+mohite2@users.noreply.github.com>
2021-06-09 18:39:14 -0500

    Assignment 01: Task 01

---
+++ Assignement.py
@@ -0,0 +1,80 @@
+# question 01
+
+a = 1 
+b= "string"
+c= 2.01
+
+type(a)
+type(b)
+type(c)
+
+# question 02: 
+
+x= 23j
+
+y = 12
+
+temp = x
+x=y
+y=temp 
+
+x
+
+## question 03: 
+
+a =12
+b= 100
+
+temp_e = a
+a=b
+b= temp_e
+print(a,b)
+
+# question 04: 
+
+input("insert the values")
+
+raw_input("insert the values")
+
+## question 05: 
+
+x = [int(input("Enter the number between 0-10:  ")) for x in range(0,2)]
+
+z = sum(x)
+result = z+30 
+
+result 
+
+
+## question 06: 
+
+m = input("Enter String: ")
+n = int(input("enter interger: "))
+o = float(input("enter float"))
+
+print("data type of m is {}".format(type(m)))
+print("data type of n is {}".format(type(n)))
+print("data type of o is {}".format(type(o)))
+
+
+## question 07: 
+x = input("Enter the string:  ")
+
+print(x.capitalize())
+
+x = input("Enter the string:  ")
+print(x.lower())
+
+x = input("Enter the string:  ")
+print(x.upper())
+
+##question 08: 
+a = 123
+
+print(type(a))
+
+a = "DSDWD"
+
+print(type(a))
+
+## the varibale is values is reassigned so data stype changes. 
